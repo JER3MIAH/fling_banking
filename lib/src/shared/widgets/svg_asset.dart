796a0f108f37225f8154166728106880fbdecd40
JER3MIAH <jeremiahbrownie@gmail.com>
2024-06-28 09:48:44 +0100

    Remove unused package and its implementation

--- lib/src/shared/widgets/svg_asset.dart
+++ lib/src/shared/widgets/svg_asset.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:lottie/lottie.dart';
 
 class SvgAsset extends StatelessWidget {
   final String assetName;
@@ -31,26 +30,6 @@ class SvgAsset extends StatelessWidget {
                 BlendMode.srcIn,
               ),
       ),
-    );
-  }
-}
-
-class LottieAsset extends StatelessWidget {
-  final String assetPath;
-  final double? height, width;
-  const LottieAsset({
-    super.key,
-    required this.assetPath,
-    this.height,
-    this.width,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Lottie.asset(
-      assetPath,
-      height: height,
-      width: width,
     );
   }
 }
